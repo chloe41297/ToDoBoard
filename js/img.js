45e6536220efcf182df23b928a28d539a0ef1312
chloe41297 <79011228+chloe41297@users.noreply.github.com>
2021-05-02 23:03:45 +0900

    Updated random imgs w/ unsplash

---
+++ js/img.js
@@ -0,0 +1,12 @@
+const IMG1 = document.querySelector("#img1");
+
+function handleLink(){
+    //IMG1.style.filter = "brightness(70%)";
+   
+}
+
+function init(){
+    IMG1.addEventListener("mouseover",handleLink);
+
+}
+init();
