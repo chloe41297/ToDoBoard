01ead4ce41a5c124404c5b3d165447f64b57a386
chloe41297 <79011228+chloe41297@users.noreply.github.com>
2021-05-03 04:32:19 +0900

    Updated scroll w/ interactive windowsize

--- js/scroll.js
+++ js/scroll.js
@@ -4,30 +4,25 @@ const nav3 = document.querySelector(".nav__dot3");
 const nav4 = document.querySelector(".nav__dot4");
 const nav5 = document.querySelector(".nav__dot5");
 const SCROLL = document.querySelector(".scroll");
+const windowSize = window.innerWidth;
 
 function handleNav1(){
     window.scrollTo(0,0);
 }
 function handleNav2(){
-    window.scrollTo(1450,0);
+    window.scrollTo(windowSize,0);
 }
 function handleNav3(){
-    window.scrollTo(2900,0);
+    window.scrollTo(2*windowSize,0);
 }
 function handleNav4(){
-    window.scrollTo(4350,0);
+    window.scrollTo(3*windowSize,0);
 }
 function handleNav5(){
-    window.scrollTo(5800,0);
+    window.scrollTo(4*windowSize,0);
 }
 
-function handleScroll(){
-    
-    if(window.scrollX <1000){
-        nav1.style.color="var(--main--blue)";
-        
-    }
-}
+
 
 function init(){
    nav1.addEventListener("click",handleNav1);
@@ -35,7 +30,6 @@ function init(){
    nav3.addEventListener("click",handleNav3);
    nav4.addEventListener("click",handleNav4);
    nav5.addEventListener("click",handleNav5);
-   handleScroll();
-   console.log(window.scrollX);
+  
 }
 init();
